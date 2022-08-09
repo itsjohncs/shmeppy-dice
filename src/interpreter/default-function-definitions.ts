1da3aa8cc013bc819af5ce265175d87af64eb07d
itsjohncs <johnsullivan.pem@gmail.com>
2022-08-08 22:28:06 -0700

    Add dbldice() function.

--- src/interpreter/default-function-definitions.ts
+++ src/interpreter/default-function-definitions.ts
@@ -1,4 +1,7 @@
+import * as Ast from '../ast';
+import { InterpreterError } from './error-message.class';
 import { FunctionDefinitionList } from './function-definition-list.class';
+import { DiceInterpreter } from './dice-interpreter.class';
 
 export const DefaultFunctionDefinitions = new FunctionDefinitionList();
 
@@ -20,4 +23,26 @@ DefaultFunctionDefinitions['round'] = (interpreter, functionNode, errors) => {
 
 DefaultFunctionDefinitions['sqrt'] = (interpreter, functionNode, errors) => {
   return Math.sqrt(interpreter.evaluate(functionNode.getChild(0), errors));
+};
+
+class DblDiceInterpreterProxyHandler implements ProxyHandler<DiceInterpreter> {
+  static evaluateDiceStub(this: DiceInterpreter, target: DiceInterpreter,
+      expression: Ast.ExpressionNode, errors: InterpreterError[]): number {
+    return target.evaluateDice(expression, errors) * 2;
+  }
+
+  get(target: DiceInterpreter, p: PropertyKey, receiver: any): any {
+    if (p === 'evaluateDice') {
+      return DblDiceInterpreterProxyHandler.evaluateDiceStub.bind(this, target);
+    }
+
+    return target[p];
+  }
+}
+
+const dblDiceInterpreterProxyHandler = new DblDiceInterpreterProxyHandler();
+
+DefaultFunctionDefinitions['dbldice'] = (interpreter, functionNode, errors) => {
+  const proxiedInterpreter = new Proxy(interpreter, dblDiceInterpreterProxyHandler);
+  return proxiedInterpreter.evaluate(functionNode.getChild(0), errors);
 };
