ccc33d8c85dd25fe8820469184d7f6a256cbc653
itsjohncs <johnsullivan.pem@gmail.com>
2022-08-17 17:40:31 -0700

    Add utility for trying rolls on command line.

---
+++ roll.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+    echo "USAGE: $0 ROLL_SPEC"
+    exit 1
+fi
+
+SCRIPT_DIR="$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)"
+
+cd "$SCRIPT_DIR/src"
+
+ts-node 4<<< "$1" <<EOM
+import {Dice} from ".";
+import {inspect} from "util";
+import {readFileSync} from "fs";
+
+// Reads from file descriptor 4
+const rollSpec = readFileSync(4).toString();
+
+console.log(inspect((new Dice()).roll(rollSpec), false, null))
+EOM
